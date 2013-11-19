270a683d00d819d0d3d1831640adfc154987eb64
zahkaria <zak@rubberpigeon.com>
2013-11-19 20:58:18 +1100

    Can't get MonketJSTest Working
    Checked tests and each should return value as expect
    but MONKEYJSTEST doesn't even work on there test site
    http://monkeytestjs.io/tests/ will have to submit test and explain

--- TheMonkeys-MonkeytestJS-7332a8c/tests/page/formTest_page_test.js
+++ TheMonkeys-MonkeytestJS-7332a8c/tests/page/formTest_page_test.js
@@ -2,21 +2,18 @@ registerTest ('The form cannot be submitted if any of the required fields are em
   {
       setup:function (container) {
           // this is run before the test
-          // use it to do things like clear cookies or assign helpers
-          this.form = getElementById('formTest');
-          this.inputs = form.getElementsByTagName('input');
-          
+          // use it to do things like clear cookies or assign helpers         
           this.blankForm = function(){
+            inputs = $('#formTest input');
             for (var i = inputs.length - 1; i >= 0; i--) {
               inputs[i].value = ''
             };  
           };
       }
       ,load : function () {
-          // this is the test script
           this.blankForm();
           this.test('Blank Form', function($) {
-            ok(!form.submit, 'Form should not submit blank')
+            ok($('#formTest #submitForm').click(), 'Form should not submit blank')
           });
       }
   }
@@ -24,18 +21,15 @@ registerTest ('The form cannot be submitted if any of the required fields are em
 registerTest ('The form cannot be submitted if the enquiry type is "Product complaint" and product name, product size, use-by date and batch code are empty.',
   {
       setup:function (container) {
-          // this is run before the test
-          // use it to do things like clear cookies or assign helpers
           this.trigerProductComplaint = function(){
             var enquiryType = $('#formTest #enquiryType');
-            enquiryType.options.selectedIndex = 2;
+            enquiryType.prop('selectedIndex',2);
             enquiryType.trigger('onblur');
           }
       ,load : function () {
-          // this is the test script
           this.trigerProductComplaint();
           this.test('Blank Form with enquiry type == product complaint', function($) {
-            ok(!form.submit, 'Form should not submit blank')
+            ok($('#formTest #submitForm').click(), 'Form should not submit blank')
           });
       }
   }
